cca5f2aba187739b73235d7582dd18473a8cdbe6
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2018-11-09 17:39:07 +0900

    Contentfulの組み込み

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -2,17 +2,50 @@
   <section class="container">
     <div>
       <nuxt-link to="/articles/" class="button--grey">ブログ一覧へ戻る</nuxt-link>
-      <h1 class="title">タイトル</h1>
+      <div class="title">
+        <h1>{{ currentPost.fields.title }}</h1>
+        <p>公開日：{{ currentPost.fields.publishDate }}</p>
+      </div>
       <div class="text">
-        本文
+        {{ currentPost.fields.mainText }}
       </div>
     </div>
   </section>
 </template>
 
 <script>
+// contentfulの宣言
+import { createClient } from '~/plugins/contentful.js';
+// 設定情報の取得
+const client = createClient();
+
 export default {
-}
+  // 変数の宣言
+  data() {
+    return {
+      allPosts: [],
+      currentPost: [],
+    };
+  },
+  // データの取得(非同期)
+  asyncData({ env, params }) {
+    return client
+      .getEntries({
+        'content_type': env.CTF_BLOG_ID,
+      })
+      .then((entries) => {
+        const posts = entries.items;
+        const current = posts.filter(function(item) {
+          return item.fields.slug === params.slug;
+        });
+        return {
+          allPosts: posts,
+          currentPost: current[0],
+        };
+      })
+      .catch(console.error);
+  },
+};
 </script>
 
 <style lang="css" scoped>
